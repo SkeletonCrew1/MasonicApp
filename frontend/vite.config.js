6a33efc231fa60baae9470e4120a2e0e151ab752
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-07-22 13:35:26 +0300

    fix: resolve spacing conflict in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-<<<<<<< HEAD
-    plugins: [vue()],
-    server: {
-        host: true,
-        port: 5173,
-    },
-=======
   plugins: [vue()],
   server: {
     host: true,
     port: 5173,
   },
->>>>>>> fix/SKEL2-20-implement-broadcast-feature
 });
