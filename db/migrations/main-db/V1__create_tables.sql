7c531accb73ca0c79a87726d858ae9d9285e2975
OlehKonovaliuk <konovalukoleg0@gmail.com>
2026-07-25 19:25:37 +0300

    Skel2-27-db-migration (#15)

---
+++ db/migrations/main-db/V1__create_tables.sql
@@ -0,0 +1,22 @@
+create table if not exists Users (
+    UserId serial primary key,
+    UserDisplayName varchar(40) not null,
+    UserPassword text not null,
+    UserStatus varchar(40) not null,
+    UserEmail text not null,
+    UserIsInquisitor bool not null
+);
+
+create table if not exists BannedIps (
+    UserID int primary key references Users(UserID) not null,
+    BannedIp varchar(40) not null
+);
+
+create table if not exists Messages (
+    MessageId serial primary key not null,
+    MessageContext text not null,
+    MessageRecieverStatus varchar(40) not null,
+    MessageStatus varchar(20) not null,
+    SenderId int not null references Users(UserId) not null,
+    CreatedAt timestamp with time zone not null
+);
