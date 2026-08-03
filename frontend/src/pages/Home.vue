4ca04ad7f16f1dd27153f626538a48e8d3b7f2b9
Riot <romanmissuryn@gmail.com>
2026-08-03 11:59:51 +0300

    SKEL2-Added-control-panel-page-butoon

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="page">
-    <nav class="navbar">
+<nav class="navbar">
       <div class="logo">Cult of the Tree</div>
       <ul class="nav-links">
+        <li v-if="userStatus === 'golden'">
+          <router-link to="/control-panel">Control Panel</router-link>
+        </li>
         <li>
-          <a href="#" @click="Logout">Logout</a>          
+          <a href="#" @click="Logout">Logout</a>         
         </li>
       </ul>
     </nav>
