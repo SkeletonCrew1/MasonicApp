6f11b4431d9893e89c06944fb6b940b135bbfc01
Vitalik23657 <vitaliyoliyniyk@gmail.com>
2026-08-05 18:25:49 +0300

    SKEL2-68: create IAM users for the team develepment using terraform

---
+++ terraform/iam-users/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  users = [
+    "Roman_Misiuryn",
+    "Vitalii_Oliinyk",
+    "Oleh_Konovaliuk",
+    "Anastasia_Kosheleva",
+    "Maksym_Klimukh"
+  ]
+}
---
+++ terraform/iam-users/outputs.tf
@@ -0,0 +1,11 @@
+output "user_passwords" {
+  value = {
+    for user_key, profile in aws_iam_user_login_profile.users :
+    user_key => profile.password
+  }
+  description = "A map of usernames to their initial temporary passwords"
+  sensitive   = true
+}
+
+# Command to output credentials to a json file
+# terraform output -json user_passwords > passwords.json
