c88c8421a76be2ede4182bc9f6c0dfbbf2cad2b8
OlehKonovaliuk <konovalukoleg0@example.com>
2026-08-06 17:36:17 +0300

    SKEL2-64-first-try

--- frontend/src/pages/Registration.vue
+++ frontend/src/pages/Registration.vue
@@ -102,7 +102,7 @@ const router = useRouter();
 const user_email = ref("");
 const user_password = ref("");
 const daily_password = ref("");
-const AUTH_SERVICE_URL = import.meta.env.VITE_AUTH_SERVICE_URL
+
 
 async function Register() {
     const user = {
@@ -112,7 +112,7 @@ async function Register() {
     };
     
     try {
-        const response = await fetch(`${AUTH_SERVICE_URL}/register`, {
+        const response = await fetch(`/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
+
+const AUTH_SERVICE = env.VITE_AUTH_SERVICE_URL;
+
 export default defineConfig({
-  plugins: [vue()],
+
   server: {
-    host: true,
-    port: 5173,
+    proxy: {
+      '/register': AUTH_SERVICE + '/register',
+    },
   },
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        // Enable JSX
+        isCustomElement: (tag) => tag.includes('-')
+      }
+    }
+  })],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  }
 });
