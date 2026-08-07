61f7e1139d1d4968695f3a54bd38b9f29d4617c6
OlehKonovaliuk <konovalukoleg0@example.com>
2026-08-07 17:16:19 +0300

    SKEL2-64-fixed-misspelling

--- db/migrations/main-db/V1.1__add_test_data.sql
+++ db/migrations/main-db/V1.1__add_test_data.sql
@@ -7,4 +7,4 @@ VALUES ('10.10.10.10.');
 
 
 INSERT INTO messages ( MessageContext, MessageRecieverStatus, MessageStatus, SenderId,CreatedAt)
-VALUES ('bob died',  'golden', 'sent', '1','2013-11-03 00:00:00-07');
+VALUES ('bob died',  'gold', 'sent', '1','2013-11-03 00:00:00-07');
