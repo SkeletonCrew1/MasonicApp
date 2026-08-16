efd6915d66a2b6db78a628cbd0000f3e99ca98a2
OlehKonovaliuk <konovalukoleg0@example.com>
2026-08-16 21:28:47 +0300

    SKEL2-74-routing-fix

--- backend-django/core/urls.py
+++ backend-django/core/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/backend/", include("api.urls")),
 ]
