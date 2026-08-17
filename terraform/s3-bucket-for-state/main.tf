ee259fe79f485096c6fa3c8bad8cf5f12df301e5
OlehKonovaliuk <konovalukoleg0@example.com>
2026-08-18 00:27:48 +0300

    SKEL2-75-styling

--- terraform/s3-bucket-for-state/main.tf
+++ terraform/s3-bucket-for-state/main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "masonicapp-terraform-state-dev"
+  bucket = "masonicapp-terraform-state"
 
   tags = {
     ManagedBy = "Terraform"
-    Name      = "masonicapp-terraform-state-dev"
+    Name      = "masonicapp-terraform-state"
   }
 }
 
