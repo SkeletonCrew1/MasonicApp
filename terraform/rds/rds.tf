9815ec3f77cabb721fac9bb977301806c128a43e
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-08-17 22:54:14 +0300

    temporary accessible

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -16,4 +16,6 @@ resource "aws_db_instance" "main" {
 
     skip_final_snapshot = true
     multi_az            = false
+
+    publicly_accessible  = true
 }
--- terraform/rds/secrets.tf
+++ terraform/rds/secrets.tf
@@ -4,18 +4,39 @@ resource "random_password" "db_password" {
     override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-resource "aws_secretsmanager_secret" "db_password" {
-    name                    = "${var.env}/paranormal/db-password"
-    description             = "Pass for Paranormal RDS"
+locals {
+    db_secrets = {
+        "USERS_DB_HOST"     = aws_db_instance.main.address
+        "USERS_DB_PORT"     = "5432"
+        "USERS_DB_USER"     = var.db_user
+        "USERS_DB_PASSWORD" = random_password.db_password.result
+        "USERS_DB_NAME"     = "main_db"
+
+        "MAP_DB_HOST"       = aws_db_instance.main.address
+        "MAP_DB_PORT"       = "5432"
+        "MAP_DB_USER"       = var.db_user
+        "MAP_DB_PASSWORD"   = random_password.db_password.result
+        "MAP_DB_NAME"       = "map_db"
+
+        "VOTING_DB_HOST"       = aws_db_instance.main.address
+        "VOTING_DB_PORT"       = "5432"
+        "VOTING_DB_USER"       = var.db_user
+        "VOTING_DB_PASSWORD"   = random_password.db_password.result
+        "VOTING_DB_NAME"       = "voting_db"
+    }
+}
+
+resource "aws_secretsmanager_secret" "db_secrets" {
+    for_each                = local.db_secrets
+    name                    = each.key
+    description             = "Pass for Paranormal RDS for ${each.key}"
     recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_password" {
-    secret_id     = aws_secretsmanager_secret.db_password.id
+    for_each      = local.db_secrets
+    secret_id     = aws_secretsmanager_secret.db_secrets[each.key].id
     secret_string = jsonencode({
-        username = var.db_user
-        password = random_password.db_password.result
-        engine   = "postgres"
-        port     = 5432
+        (each.key) = each.value
      })
 }
